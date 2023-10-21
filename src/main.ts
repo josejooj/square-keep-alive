eb4eee0fca1e423e28a251e6dc6dbbf756ea58a2
josejooj <ribamarjunior@protonmail.com>
2023-10-21 12:39:24 -0300

    Now the project use the new API route GET /v2/apps/all/status

--- src/main.ts
+++ src/main.ts
@@ -1,78 +1,49 @@
-import { Application, Plans, Status, User } from "./typing";
+import { Status } from "./typing";
 import { config as ConfigEnv } from 'dotenv';
 import axios from "axios";
 
 ConfigEnv()
 
-const limit: Record<Plans, number> = {
-    free: 60,
-    basic: 60,
-    medium: 60,
-    advanced: 120,
-    senior: 120,
-    deluxe: 180,
-    orion: 360,
-    enterprise: 720
-}
-
 const log = (tag: string, msg: string) => console.log(`[${tag}] - ${msg}`)
-const square = axios.create({ baseURL: "https://api.squarecloud.app/v2", headers: { Authorization: process.env.SQUARE_API_TOKEN } })
-const divisor = +(process.env.REQUEST_LIMIT_DIVISOR || 4);
+const square = axios.create({ baseURL: "https://api.squarecloud.app/v2", headers: { Authorization: process.env.SQUARE_API_KEY } });
 
 (async () => {
 
     const { data, status } = await square.get(`/user`).catch(e => e.response);
-    const user = data.response?.user as User;
-    const apps = data.response?.applications as Application[];
+    const user = data.response?.user as { tag: string, plan: { name: string } };
 
-    if (typeof divisor !== 'number' || isNaN(divisor) || divisor % 1 !== 0 || divisor < 3) return log('ERROR', "REQUEST_LIMIT_DIVISOR must be a integer bigger than 3")
     if (status === 401) return log('ERROR', "Please, verify your API Key!")
-    if (user.plan.name === 'free') return log('ERROR', `I can't check your applications with free plan, because your unique application will be me`)
+    if (user.plan.name === 'free') log('ALERT', `Don't host me on Square Cloud with free plan.`) // maybe you are hosting on another machine
 
     log('INFO', `Started successfully! Hello ${user.tag}!`)
-    log('INFO', `I will check ${apps.length} applications.`)
-
-    for (let i = 0; i < apps.length; i++) log('INFO', `Application ${i + 1}: ID ${apps[i].id} - "${apps[i].tag}" `)
-
-    const timer = 1000 * 60 / limit[user.plan.name] * divisor;
-
-    log('INFO', `As you have the ${user.plan.name} plan, your request limit is ${limit[user.plan.name]}r/60s`);
-    log('INFO', `So i will do ${(limit[user.plan.name] / divisor).toFixed(2)}r/60s, one request every ${timer / 1000} second(s)`);
-    log('INFO', `As you have ${apps.length} applications, each application will be re-checked every ${timer / 1000 * apps.length} second(s)`);
+    log('INFO', `I will check all applications each 15 seconds`);
     log('INFO', `Starting to check now!`)
 
     async function check(i: number = 0) {
 
         try {
 
-            const app = apps[i % apps.length];
+            const { data, status } = await square.get("/apps/all/status").catch(e => e.response);
+            const response = data.response as Status[];
 
-            if (app) {
+            if (status === 401) return log('ERROR', `Please, re-configure your api key.`)
 
-                const { data, status } = await square.get("/apps/" + app.id + "/status").catch(e => e.response);
-                const response = data.response as Status;
+            for (const app of response) {
 
-                if (status === 404) {
-                    delete apps[i % apps.length];
-                    log('WARNING', `The application "${app.tag}" (ID ${app.id}) was deleted. Removing of check list.`)
-                }
+                if (app.running) continue;
 
-                if (status === 401) return log('ERROR', `Please, re-configure your api key.`)
-                if (status === 200 && !response.running) {
+                const { status } = await square.post(`/apps/${app.id}/start`).catch(e => e.response);
 
-                    const { status } = await square.post(`/apps/${app.id}/start`).catch(e => e.response);
+                if (status === 200) log('INFO', `[CHECK ${i + 1}] - The application "${app.id}" was offline, and i restarted it successfully!`);
+                else log('WARNING', `[CHECK ${i + 1}] - I received the HTTP ERROR ${status} when i tried to restart the application "${app.id}" (ID ${app.id})`)
+            
+            }            
 
-                    if (status === 200) log('INFO', `[CHECK ${i + 1}] - The application "${app.tag}" was offline, and i restarted it successfully!`);
-                    else log('WARNING', `[CHECK ${i + 1}] - I received the HTTP ERROR ${status} when i tried to restart the application "${app.tag}" (ID ${app.id})`)
-
-                }
-
-            }
-        } catch(e: any) {
+        } catch (e: any) {
             if (!e.message.includes("Cannot destructure")) throw e;
         }
 
-        setTimeout(check.bind(null, i + 1), timer);
+        setTimeout(check.bind(null, i + 1), 1000 * 16);
     }
 
     check();
--- src/typing.d.ts
+++ src/typing.d.ts
@@ -1,54 +1,10 @@
-type User = {
-    id: string,
-    tag: string,
-    locale: string,
-    email: string,
-    blocklist: boolean
-    plan: {
-        name: Plans,
-        memory: {
-            limit: number,
-            available: number,
-            used: number
-        },
-        duration: {
-            formated: string,
-            raw: number
-        }
-    }
-}
-
-type Application = {
-    id: string,
-    tag: string,
-    ram: number,
-    lang: string,
-    type: string,
-    cluster: string,
-    isWebsite: boolean,
-    avatar: string
-}
-
 type Status = {
+    id: string,
     cpu: string,
     ram: string,
-    status: string,
-    running: boolean,
-    storage: string,
-    network: {
-        total: string,
-        now: string
-    },
-    requests: number,
-    uptime: number,
-    time: number
+    running: boolean
 }
 
-type Plans = 'free' | 'basic' | 'medium' | 'advanced' | 'senior' | 'deluxe' | 'orion' | 'enterprise'
-
 export {
-    User,
-    Application,
-    Plans,
     Status
 }
