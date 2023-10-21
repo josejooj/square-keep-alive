8a0bfa46bec2fe15ddbf3948032a232a27f4199a
josejooj <ribamarjunior@protonmail.com>
2023-10-21 14:26:40 -0300

    Now you can send the alerts with a discord webhook

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,10 @@ ConfigEnv()
 
 const log = (tag: string, msg: string) => console.log(`[${tag}] - ${msg}`)
 const square = axios.create({ baseURL: "https://api.squarecloud.app/v2", headers: { Authorization: process.env.SQUARE_API_KEY } });
+const discord = axios.create({ baseURL: "https://discord.com/api/v10/" })
 
 square.interceptors.response.use(res => res, error => error.response);
+discord.interceptors.response.use(res => res, error => error.response);
 
 (async () => {
 
@@ -32,26 +34,47 @@ square.interceptors.response.use(res => res, error => error.response);
 
             if (status === 401) return log('ERROR', `Please, re-configure your api key.`)
 
+            /**
+             * I could use "of response.filter(app => !app.running)" here,
+             * but this is unperformatic, because i'm already doind a loop, i dont need another loop (filter is a loop)
+             */
+
             for (const app of response) {
 
                 if (app.running) continue;
 
                 const { data } = await square.get(`/apps/${app.id}/logs`);
                 const { status } = await square.post(`/apps/${app.id}/start`);
- 
-                if (data.status === 'success') fs.writeFileSync(`./logs/[${new Date().getTime()}] - ${app.id}.log`, data.response.logs)
-                if (status === 200) log('INFO', `[CHECK ${i + 1}] - The application "${app.id}" was offline, and i restarted it successfully!`);
-                else log('WARNING', `[CHECK ${i + 1}] - I received the HTTP ERROR ${status} when i tried to restart the application "${app.id}" (ID ${app.id})`)
+                const body: Record<string, any> = {};
 
+                if (data.status === 'success') fs.writeFileSync(`./logs/[${new Date().getTime()}] - ${app.id}.log`, data.response.logs)
+                if (status === 200) {
+                    log('INFO', `[CHECK ${i + 1}] - The application "${app.id}" was offline, and i restarted it successfully!`);
+                    body.embeds = [{
+                        color: 0x00FF00,
+                        description: `**The application \`"${app.id}"\` was offline, and i restarted it successfully!**`
+                    }]
+                }
+                else {
+                    log('WARNING', `[CHECK ${i + 1}] - I received the HTTP ERROR ${status} when i tried to restart the application "${app.id}"`)
+                    body.embeds = [{
+                        color: 0xFFFF00,
+                        description: `**I received the HTTP ERROR \`${status}\` when i tried to restart the application \`${app.id}\`**`
+                    }]
+                }
+
+                if ('DISCORD_WEBHOOK_ID' in process.env && 'DISCORD_WEBHOOK_TOKEN' in process.env) {
+                    await discord.post(`/webhooks/${process.env.DISCORD_WEBHOOK_ID}/${process.env.DISCORD_WEBHOOK_TOKEN}`, body)
+                }
             }
-            
+
             ++i
 
         } catch (e: any) {
             if (!e.message.includes("Cannot destructure")) throw e;
         }
 
-        setTimeout(check.bind(null, i), 1000 * 16);
+        setTimeout(check.bind(null, i), 1000 * 15);
     }
 
     check();
