d5ddd664ae7491b117a0df3907882626973be515
josejooj <josejooj.dev@gmail.com>
2024-12-27 00:34:11 -0300

    refactor: refactor log

--- src/constants.ts
+++ src/constants.ts
@@ -2,3 +2,15 @@ export const
     square_url = "https://api.squarecloud.app/v2",
     discord_url = "https://discord.com/api/v10/",
     square_init = { headers: { Authorization: process.env.SQUARE_API_KEY! } };
+
+type log_class = 'error' | 'warn' | 'info'
+
+const log_colors: Record<log_class, string> = {
+    error: "31m",
+    warn: "33m",
+    info: "36m"
+}
+
+export const Log = (level: log_class, msg: string) => {
+    console[level](`\x1b[${log_colors[level]}[${level}] - ${msg}\x1b[0m`);
+}
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,16 @@
-import { square_init, square_url } from "./constants";
+import { Log, square_init, square_url } from "./constants";
 import fs from 'fs';
 
-const log = (tag: string, msg: string) => console.log(`[${tag}] - ${msg}`);
 const init = async () => {
 
     const user_req = await fetch(`${square_url}/users/me`, square_init);
     const user = await user_req.json().then(r => r.response.user) as { name: string, plan: { name: string } };
 
-    if (user_req.status === 401) return log('ERROR', "Please, verify your API Key!")
-    if (user.plan.name === 'free') log('ALERT', `Don't host me on Square Cloud with free plan.`) // maybe you are hosting on another machine
+    if (user_req.status === 401) return Log('error', "Please, verify your API Key!")
+    if (user.plan.name === 'free') Log('warn', `Don't host me on Square Cloud with free plan.`) // maybe you are hosting on another machine
     if (!fs.existsSync("./logs")) fs.mkdirSync("./logs")
 
-    log('INFO', `Started successfully! Hello ${user.name}!`)
-    log('INFO', `I will check all applications each 15 seconds`);
-    log('INFO', `Starting to check now!`)
+    Log('info', `Started successfully! Hello ${user.name}!`);
 
 }
 
